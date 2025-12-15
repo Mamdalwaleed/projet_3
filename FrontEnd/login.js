b6f7eb27856cba20cf7c10e6e8b6681903cd1882
mamdal <waleedmamdal96@gmail.com>
2025-12-15 11:50:07 +0100

    add login html et js

---
+++ FrontEnd/login.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+
+    const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password })
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+
+        // Stockage du token
+        localStorage.setItem("token", data.token);
+
+        // Redirection vers l’accueil
+        window.location.href = "index.html";
+    } else {
+        alert("Erreur dans l’identifiant ou le mot de passe");
+    }
+});
